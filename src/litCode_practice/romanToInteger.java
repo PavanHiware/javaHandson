23c47fe2002037e6a68a20560d4dc91d3e9e60d0
Hiware <2110883@cognizant.com>
2026-04-06 21:07:20 +0530

    enhanced the code for better validation

--- src/litCode_practice/romanToInteger.java
+++ src/litCode_practice/romanToInteger.java
@@ -8,33 +8,52 @@ public class romanToInteger {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Please enter the no. in ROMAN style : ");
 		String userInput = sc.nextLine();
-				
+
 		sc.close();
 		return userInput;
 	}
+
 //convert the roman value to integer
 	static int convertRomanTonum(String romanString) {
-		int requiredNum=0;
-		for(int i = 0; i<romanString.length();i++) {
+		int result = 0;
+		int preValue=0;
+		int currentValue=0;
+		
+		for (int i = romanString.length() -1 ; i >=0 ; i--) {
 			char c = romanString.charAt(i);
-			switch (c) {
-			case 'I': requiredNum++;
-					break;
-			case 'V': requiredNum = requiredNum + 5;
-			break;
-			case 'X': requiredNum = requiredNum + 10;
-			break;
 			
+
+			switch (c) {
+			case 'I':
+
+				currentValue =1;
+				break;
+
+			case 'V':
+				currentValue =  5;
+				break;
+			case 'X':
+				currentValue = 10;
+				break;
+
 			}
+			if(currentValue<preValue) {
+				result -= currentValue;
+			}else {
+				result += currentValue;
 				
+			}
+			preValue =currentValue;	
+
 		}
-		
-		return requiredNum;
-		
+
+		return result;
+
 	}
+
 	public static void main(String[] args) {
 		String input = userInputString();
 		int num = convertRomanTonum(input);
-		System.out.printf("Required number = %d ",num);
+		System.out.printf("Required number = %d ", num);
 	}
 }
